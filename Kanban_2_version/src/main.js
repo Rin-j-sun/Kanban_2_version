cff2f0e3359a7e950a64b401838fda0ac4b3f441
god <sabrina.sotnikova@gmail.com>
2024-02-04 20:40:59 +0700

    Добавление компонента формы добавления задачи

--- Kanban_2_version/src/main.js
+++ Kanban_2_version/src/main.js
@@ -1,3 +1,50 @@
+Vue.component('task-form', {
+    props: [],
+    template: `
+    <div class="content_form">
+        <form @submit.prevent="addTask">
+          <div class="new_text">
+            <label for="task-name">Заголовок новой задачи:</label>
+            <input class="input" id="task-name" type="text" v-model="taskName"><br><br></div>
+          <div class="new_desc">  
+          <label for="task-desc">Описание задачи:</label>
+            <textarea id="task-desc" v-model="description"></textarea><br><br></div>
+          <div class="new_deadline">
+          <label for="deadline">Срок сдачи:</label>
+            <input type="date" id="deadline" v-model="deadline" name="deadline-task" min="2024-01-01" max="2025-12-31" required />
+            <button type="submit">Создать</button>
+          </div>
+        </form>
+    </div>
+    `,
+    data() {
+        return {
+            taskName: '',
+            description: '',
+            deadline: ''
+        };
+    },
+    methods: {
+        addTask() {
+            if (this.taskName !== '') {
+                const newTask = {
+                    title: this.taskName,
+                    description: this.description,
+                    deadline: this.deadline,
+                    reason: ''
+                };
+                newTask.createdDate = new Date().toLocaleDateString();
+                this.$emit('add', newTask);
+                this.taskName = '';
+                this.description = '';
+                this.deadline = '';
+            } else {
+                alert("Введите название задачи");
+            }
+        }
+    }
+});
+
 Vue.component('task', {
     props: ['task', 'type'],
     data() {
